6fa22b4ebb8ec4c6ae1ccb1636c90498bb193bd5
David Garske <david@wolfssl.com>
2019-05-01 11:24:47 -0700

    More python3 changes.

--- make/osx/build_wheels.sh
+++ make/osx/build_wheels.sh
@@ -5,13 +5,13 @@ for PYVERSION in 2.7 3.6 3.7; do
     virtualenv -p /Library/Frameworks/Python.framework/Versions/${PYVERSION}/bin/python${PYVERSION} venv_${PYVERSION}
     . ./venv_${PYVERSION}/bin/activate
     curl https://bootstrap.pypa.io/get-pip.py | python
-    pip install -r requirements/setup.txt
-    python setup.py bdist_wheel
-    pip install -r requirements/test.txt
+    pip3 install -r requirements/setup.txt
+    python3 setup.py bdist_wheel
+    pip3 install -r requirements/test.txt
     set +e
-    pip uninstall -y wolfcrypt
+    pip3 uninstall -y wolfcrypt
     set -e
-    pip install wolfcrypt --no-index -f dist
+    pip3 install wolfcrypt --no-index -f dist
     rm -rf tests/__pycache__
     py.test tests
     deactivate
